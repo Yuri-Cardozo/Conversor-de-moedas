98955d1bf1b85695cca347ea8e47bfb6f9cc7e73
Yuri Cardozo <103226844+Netunosz@users.noreply.github.com>
2023-11-13 16:54:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var valorEmDolar = 30;
+var cotacaoDoDolar = 4.91; // use o ponto como separador de números //
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar
+
+valorEmReal = valorEmReal.toFixed(2);
+
+alert("R$ " + valorEmReal);
